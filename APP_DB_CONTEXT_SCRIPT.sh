695f21ae8e614fe61171b9def43003296c5c981e
David-Akobiya <david.akobiya@gmail.com>
2026-04-23 19:14:33 +0300

    breaking changes

--- APP_DB_CONTEXT_SCRIPT.sh
+++ APP_DB_CONTEXT_SCRIPT.sh
@@ -5,4 +5,3 @@ dotnet ef migrations add mew_migration_Infrastructure_6 -c AppDbContext --projec
 dotnet ef database update -c AppDbContext --project /Users/akobiyada/Documents/SpecialProjects/30_PROPERTY_CAT/krugozor_backend/Krugozor.Infrastructure.Database/Krugozor.Infrastructure.Database.csproj
 dotnet ef migrations script -c AppDbContext --project /Users/akobiyada/Documents/SpecialProjects/30_PROPERTY_CAT/krugozor_backend/Krugozor.Infrastructure.Database/Krugozor.Infrastructure.Database.csproj
 rm -rf /Users/akobiyada/Documents/SpecialProjects/30_PROPERTY_CAT/krugozor_backend/Krugozor.Infrastructure.Database/Migrations
-
