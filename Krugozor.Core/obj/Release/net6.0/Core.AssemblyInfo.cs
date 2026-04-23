1feeadc299f60c008ee5b055ead47cc32f7c6af4
David-Akobiya <david.akobiya@gmail.com>
2026-04-23 20:09:59 +0300

    breaking changes

--- Krugozor.Core/obj/Release/net6.0/Core.AssemblyInfo.cs
+++ Krugozor.Core/obj/Release/net6.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71a493aaec212d1d43760f896ac9b0f15339f135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffcdd58429ee3b4a630350fe09af9a45545f670c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Krugozor.Infrastructure.Strapi/StrapiService/StrapiService.cs
+++ Krugozor.Infrastructure.Strapi/StrapiService/StrapiService.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 using RestSharp;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Krugozor.Infrastructure.Strapi
 {
@@ -12,6 +13,28 @@ namespace Krugozor.Infrastructure.Strapi
     public StrapiService(IConfiguration config)
     {
       _config = config;
+    }
+
+    private void WriteDebugLog(string runId, string hypothesisId, string location, string message, object data)
+    {
+      try
+      {
+        var payload = new
+        {
+          sessionId = "ea380a",
+          runId,
+          hypothesisId,
+          location,
+          message,
+          data,
+          timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
+        };
+        File.AppendAllText("/Users/akobiyada/Documents/SpecialProjects/01_Focus_for_developing/58_DAVINCI/.cursor/debug-ea380a.log", JsonConvert.SerializeObject(payload) + Environment.NewLine);
+      }
+      catch
+      {
+        // ignore debug logging failures
+      }
     }
 
 
@@ -30,11 +53,20 @@ namespace Krugozor.Infrastructure.Strapi
       if (response.Content is not null)
       {
         var result = JsonConvert.DeserializeObject<StrapiProposalContractDto>(response.Content);
-        return result.Data;
+        if (result?.Data is not null) return result.Data;
+        #region agent log
+        WriteDebugLog("pre-fix", "H1", "StrapiService.cs:GetCompanyEntity", "deserialized response has null Data", new { id, response.StatusCode, response.ContentLength });
+        #endregion
+        return new DataDto();
       }
 
       else
-        return null;
+      {
+        #region agent log
+        WriteDebugLog("pre-fix", "H1", "StrapiService.cs:GetCompanyEntity", "response content is null", new { id, response.StatusCode, response.ErrorMessage });
+        #endregion
+        return new DataDto();
+      }
     }
 
 
@@ -78,11 +110,20 @@ namespace Krugozor.Infrastructure.Strapi
       if (response.Content is not null)
       {
         var result = JsonConvert.DeserializeObject<StrapiProposalContractDto>(response.Content);
-        return result.Data!;
+        if (result?.Data is not null) return result.Data;
+        #region agent log
+        WriteDebugLog("pre-fix", "H2", "StrapiService.cs:GetProposalEntity", "deserialized response has null Data", new { id, response.StatusCode, response.ContentLength });
+        #endregion
+        return new DataDto();
       }
 
       else
-        return null;
+      {
+        #region agent log
+        WriteDebugLog("pre-fix", "H2", "StrapiService.cs:GetProposalEntity", "response content is null", new { id, response.StatusCode, response.ErrorMessage });
+        #endregion
+        return new DataDto();
+      }
     }
 
 
@@ -198,7 +239,11 @@ namespace Krugozor.Infrastructure.Strapi
       // Console.WriteLine(json);
       Console.WriteLine("==========================================================================");
       Console.WriteLine("==========================================================================");
-      return response.Content;
+      if (response.Content is not null) return response.Content;
+      #region agent log
+      WriteDebugLog("pre-fix", "H3", "StrapiService.cs:AddImageToMediaLibrary", "upload response content is null", new { filePath, response.StatusCode, response.ErrorMessage });
+      #endregion
+      return string.Empty;
     }
 
 
